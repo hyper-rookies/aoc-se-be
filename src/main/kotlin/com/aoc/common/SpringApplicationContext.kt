54d7ce961fa48012c8134bc8c8321408af486690
saokiritori <soeun-lee@nhnad.com>
2026-04-08 11:30:35 +0900

    [feat] ApplicationContextAware 브릿지

--- src/main/kotlin/com/aoc/common/SpringApplicationContext.kt
+++ src/main/kotlin/com/aoc/common/SpringApplicationContext.kt
@@ -1,2 +1,19 @@
 package com.aoc.common
 
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.stereotype.Component
+
+@Component
+class SpringApplicationContext : ApplicationContextAware {
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        Companion.applicationContext = applicationContext
+    }
+
+    companion object {
+        private lateinit var applicationContext: ApplicationContext
+
+        fun <T> getBean(beanClass: Class<T>): T = applicationContext.getBean(beanClass)
+    }
+}
